6dfda52847eb7a2ac324b11cfbe6916589c594d8
manuelbarral <manuelbarral02@gmail.com>
2019-06-13 22:41:42 -0300

    Created ApiManager, ModelManager and Api constants

---
+++ iOS-movie-project/Models/ApiManager.swift
@@ -0,0 +1,17 @@
+//
+//  ApiManager.swift
+//  iOS-movie-project
+//
+//  Created by Manu on 13/6/19.
+//  Copyright © 2019 Manuel Barral. All rights reserved.
+//
+
+import Foundation
+
+class ApiManager {
+    
+    //Singleton property
+    static let sharedInstance = ApiManager()
+    
+    private init() {}
+}
