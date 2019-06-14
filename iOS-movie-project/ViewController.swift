1e47593d342c7437a8570b9d5cdd8a848b64423f
juanmanuelbarral <manuelbarral02@gmail.com>
2019-06-13 21:56:51 -0300

    Initial Commit

---
+++ iOS-movie-project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-movie-project
+//
+//  Created by Manu on 13/6/19.
+//  Copyright © 2019 Manuel Barral. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
