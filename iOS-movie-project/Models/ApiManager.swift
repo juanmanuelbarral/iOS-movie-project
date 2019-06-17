d199b4ffabb98801fabbb7e4ddc2a3791ea7cbf6
manuelbarral <manuelbarral02@gmail.com>
2019-06-16 23:03:21 -0300

    Started Api manager and Api constants

--- iOS-movie-project/Models/ApiManager.swift
+++ iOS-movie-project/Models/ApiManager.swift
@@ -14,4 +14,46 @@ class ApiManager {
     static let sharedInstance = ApiManager()
     
     private init() {}
+    
+    // GET POPULAR MOVIES
+    // https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1
+    func getPopularMovies() {
+        // TODO: implement
+    }
+    
+    // GET MOVIE DETAILS
+    // https://api.themoviedb.org/3/movie/{movie_id}?api_key=<<api_key>>&language=en-US
+    func getDetailsMovie(movieId: Int) {
+        // TODO: implement
+    }
+    
+    // GET MOVIE CREDITS
+    // https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key=<<api_key>>
+    func getCreditsMovie(movieId: Int) {
+        // TODO: implement
+    }
+    
+    // GET SIMILAR MOVIES
+    // https://api.themoviedb.org/3/movie/{movie_id}/similar?api_key=<<api_key>>&language=en-US&page=1
+    func getSimilarMovies(movieId: Int) {
+        // TODO: implement
+    }
+    
+    // GET PERSON DETAILS
+    // https://api.themoviedb.org/3/person/{person_id}?api_key=<<api_key>>&language=en-US
+    func getDetailsPerson(personId: Int) {
+        // TODO: implement
+    }
+    
+    // GET PERSONS MOVIE CREDITS
+    // https://api.themoviedb.org/3/person/{person_id}/movie_credits?api_key=<<api_key>>&language=en-US
+    func getMovieCreditsPerson(personId: Int) {
+        // TODO: implement
+    }
+    
+//    // GET PERSONS TV CREDITS
+//    // https://api.themoviedb.org/3/person/{person_id}/tv_credits?api_key=<<api_key>>&language=en-US
+//    func getTvCreditsPerson(personId: Int) {
+//        // TODO: implement
+//    }
 }
