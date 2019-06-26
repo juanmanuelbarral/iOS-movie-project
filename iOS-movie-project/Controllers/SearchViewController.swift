01801a9ae24c85cc2674534dfb9aaf5a1f20237b
Manu Barral <manuelbarral02@gmail.com>
2019-06-25 23:06:09 -0300

    Navigation search screen
    Missing search performed

--- iOS-movie-project/Controllers/SearchViewController.swift
+++ iOS-movie-project/Controllers/SearchViewController.swift
@@ -11,21 +11,17 @@ import UIKit
 class SearchViewController: UIViewController {
 
     @IBOutlet weak var searchBar: UISearchBar!
-    @IBOutlet weak var searchButton: UIImageView!
+    @IBOutlet weak var searchButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        // maybe will use
     }
-    */
-
+    
+    @IBAction func onTouchSearchButton(_ sender: Any) {
+        performSegue(withIdentifier: "fromSearchToResults", sender: nil)
+    }
 }
