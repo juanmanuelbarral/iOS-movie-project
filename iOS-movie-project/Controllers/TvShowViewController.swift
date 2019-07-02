bf340b44de706257998034ddf483ea4454e9ea5c
Manu Barral <manuelbarral02@gmail.com>
2019-07-02 00:43:03 -0300

    Tv show in Main storyboard

---
+++ iOS-movie-project/Controllers/TvShowViewController.swift
@@ -0,0 +1,36 @@
+//
+//  TvShowViewController.swift
+//  iOS-movie-project
+//
+//  Created by Manu on 1/7/19.
+//  Copyright © 2019 Manuel Barral. All rights reserved.
+//
+
+import UIKit
+
+class TvShowViewController: UIViewController {
+
+    @IBOutlet weak var backdropDecoration: UIImageView!
+    @IBOutlet weak var backdropImage: UIImageView!
+    @IBOutlet weak var posterImage: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
