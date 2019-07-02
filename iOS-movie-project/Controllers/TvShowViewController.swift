18eae696b3dc6c90c28fb59f5fd124d6a1360bf9
Manu Barral <manuelbarral02@gmail.com>
2019-07-02 02:31:20 -0300

    Tv show VC with outlets and structure of code

--- iOS-movie-project/Controllers/TvShowViewController.swift
+++ iOS-movie-project/Controllers/TvShowViewController.swift
@@ -16,21 +16,90 @@ class TvShowViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var segmentControl: UISegmentedControl!
     
+    // ABOUT OUTLETS
+    @IBOutlet weak var aboutContentView: UIView!
+    @IBOutlet weak var startYearLabel: UILabel!
+    @IBOutlet weak var finishYearLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var seasonsLabel: UILabel!
+    @IBOutlet weak var episodesLabel: UILabel!
+    @IBOutlet weak var runtimeLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var creditsCollectionView: UICollectionView!
+    @IBOutlet weak var similarTvShowsCollectionView: UICollectionView!
+    
+    // SEASON OUTLETS
+    @IBOutlet weak var seasonsContentView: UIView!
+    @IBOutlet weak var seasonsCollectionView: UICollectionView!
+    
+    // PROPERTIES
+    private let apiManager = ApiManager.sharedInstance
+    private var segueItem: Any? = nil
+    private var credits: [Member] = []
+//    private var similarShows: [TvShowPreview] = []
+//    private var seasons: [Season] = []
+//    var tvShow: TvShow!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        creditsCollectionView.dataSource = self
+        creditsCollectionView.delegate = self
+        creditsCollectionView.register(UINib(nibName: "PersonViewCell", bundle: nil), forCellWithReuseIdentifier: "personCell")
+        
+        similarTvShowsCollectionView.dataSource = self
+        similarTvShowsCollectionView.delegate = self
+//        similarTvShowsCollectionView.register(UINib(nibName: "TvShowViewCell", bundle: nil), forCellWithReuseIdentifier: "tvShowCell")
+        
+        seasonsCollectionView.dataSource = self
+        seasonsCollectionView.delegate = self
+        //        similarTvShowsCollectionView.register(UINib(nibName: "TvShowViewCell", bundle: nil), forCellWithReuseIdentifier: "tvShowCell")
+        
+        posterImage.layer.borderWidth = 3
+        posterImage.layer.borderColor = UIColor(named: "Tv Show")?.cgColor
+        
+        loadImages()
+        loadInfo()
+        loadCredits()
+        loadSimilarTvShows()
+        loadSeasons()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+//        if let vC = segue.destination {
+//            do something
+//        }
     }
-    */
+    
+    private func loadImages() {}
+    
+    private func loadInfo() {}
+    
+    private func loadCredits() {}
+    
+    private func loadSimilarTvShows() {}
+    
+    private func loadSeasons() {}
 
+}
+
+extension TvShowViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+}
+
+extension TvShowViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 0, height: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // do the actions
+    }
 }
